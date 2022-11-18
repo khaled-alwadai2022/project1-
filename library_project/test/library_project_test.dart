5dff58fedf98671353a78af4c0357808b27c238a
khlaed alwadai <117672053+khaled-alwadai2022@users.noreply.github.com>
2022-11-18 12:18:14 +0300

    thired

--- library_project/test/library_project_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:library_project/library_project.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- test/addbook/test/addbook_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:addbook/addbook.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
